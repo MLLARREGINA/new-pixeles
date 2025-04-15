5d6d41fdb957f98c6e7dbc5699a7c8d70ece285f
Melani Larregina <front.developer777@gmail.com>
2025-04-15 02:48:08 -0300

    muchos cambios

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-<script>
-  const video = document.getElementById('bg-video');
-  video.addEventListener('ended', () = {
-    video.currentTime = 0};
+const video = document.getElementById('bg-video');
+  video.addEventListener('ended', () => {
+    video.currentTime = 0}) ;
     video.play();
-  {});
-</script>
+  {};
